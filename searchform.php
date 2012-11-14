8e10bd62775d1a364ff568ea7e6574c6e1116ec5
Johan Edlund <johan@edlunddesign.com>
2012-11-14 15:14:46 +0100

    rätt url för söket

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <?php 
-echo '<form role="search" method="get" id="searchform" action="' . home_url( '/' ) . '" >
+echo '<form role="search" method="get" id="searchform" action="' . bloginfo('url') . '" >
     <label class="assistive-text" for="s">' . __('Search for:') . '</label>
     <input type="text" placeholder="'.__("Sökord...").'" value="' . get_search_query() . '" name="s" id="s" />
     <input class="button" type="submit" id="searchsubmit" value="Sök" />
--- sidebar.php
+++ sidebar.php
@@ -34,7 +34,7 @@
 
 <li id="search-2" class="widget-container widget_search">
   <h3 class="widget-title">Sök</h3>
-  <form role="search" method="get" id="searchform" action="http://gransbrytning.se.preview.citynetwork.se/" >
+  <form role="search" method="get" id="searchform" action="http://gransbrytning.se/" >
   <label class="assistive-text" for="s">Sök efter:</label>
   <input type="text" placeholder="Sökord..." value="" name="s" id="s" />
   <input class="button" type="submit" id="searchsubmit" value="Sök" />
